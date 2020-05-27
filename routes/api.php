e74b8899927121308965de54f3f2ed576b8766f7
verajeevverma <rajeev.verma2@virtualemployee.com>
2020-05-27 14:55:54 +0530

    chanegs

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('get-shipping-rate','API\Fedex@getShippingrate');
+Route::post('get-shipping-rate','API\FedexController@getShippingrate');
