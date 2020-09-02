e1519efaa7cb253642169a8c1604b75e814f1774
verajeevverma <rajeev.verma2@virtualemployee.com>
2020-09-02 14:06:08 +0530

    chnages

--- app/Http/Controllers/API/FedexShipmentController.php
+++ app/Http/Controllers/API/FedexShipmentController.php
@@ -179,7 +179,8 @@ class FedexShipmentController extends Controller
         $trackingId = '';
      
         if($result->Notifications[0]->Code == 3017 || $result->Notifications[0]->Code == 3021 || $result->Notifications[0]->Code == 6541|| $result->Notifications[0]->Code == 1000){
-            $error = $result->Notifications[0]->Message;
+            // $error = $result->Notifications[0]->Message;
+            $error = 'FedEx Service Api encounter some error';
            
         }else{
 
